88fc73ef812041397d56b102fe1e0f0660747120
AdbulrhmanEldeeb <abdodebo3@gmail.com>
2024-11-02 08:38:55 +0200

    fixing heroku bugs

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 # Load environment variables (API keys, tokens, etc.)
 load_dotenv()
 # you can use only this load_dotenv()
-hf_token = os.getenv("HF_TOKEN")
+hf_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 groq_api_key = os.getenv("GROQ_API_KEY")
 # os.environ["HUGGINGFACEHUB_API_TOKEN"] = hf_token
 # os.environ["GROQ_API_KEY"] = groq_api_key
